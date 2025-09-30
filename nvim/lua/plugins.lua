a1ddcb1c795f96ca3b4d86bca742491739aadb1f
Squishmeist <ayelee02@gmail.com>
2025-09-30 09:58:22 +0100

    nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,16 +1,12 @@
--- init.lua
-
 -- ========================
 -- Bootstrap lazy.nvim
 -- ========================
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
+    "git", "clone", "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "--branch=stable",
     lazypath,
   })
 end
@@ -20,47 +16,33 @@ vim.opt.rtp:prepend(lazypath)
 -- Plugins
 -- ========================
 require("lazy").setup({
-  -- ========================
-  -- UI & Themes
-  -- ========================
+  -- ===== UI & Themes =====
   {
     'projekt0n/github-nvim-theme',
     name = 'github-theme',
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
+    lazy = false,
+    priority = 1000,
     config = function()
-      require('github-theme').setup({
-        -- ...
-      })
-
+      require('github-theme').setup({})
       vim.cmd('colorscheme github_dark_dimmed')
     end,
   },
-  { 'EdenEast/nightfox.nvim' },
-  { 'rose-pine/neovim', name = 'rose-pine-moon' },
-  { 'rebelot/kanagawa.nvim' },
   { 'nvim-lualine/lualine.nvim', dependencies = { 'nvim-tree/nvim-web-devicons' } },
   { 'nvim-tree/nvim-web-devicons' },
 
-  -- ========================
-  -- Fuzzy Finder & Utilities
-  -- ========================
+  -- ===== Fuzzy Finder & Utilities =====
   { 'nvim-telescope/telescope.nvim', tag = '0.1.5', dependencies = { 'nvim-lua/plenary.nvim' } },
   { 'theprimeagen/harpoon' },
   { 'mbbill/undotree' },
-  { 'kdheepak/lazygit.nvim', dependencies = { 'nvim-lua/plenary.nvim' } },
   { 'akinsho/toggleterm.nvim', version = "*", config = true },
   { 'elentok/format-on-save.nvim' },
-  { 'APZelos/blamer.nvim' },
-  { 'sindrets/diffview.nvim' },
+
+  -- ===== Treesitter =====
   { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
   { "nvim-treesitter/playground" },
-  { "nvim-treesitter/nvim-treesitter-context" },
   { "windwp/nvim-ts-autotag" },
 
-  -- ========================
-  -- LSP & Autocompletion
-  -- ========================
+  -- ===== LSP & Autocompletion =====
   { 'williamboman/mason.nvim' },
   { 'williamboman/mason-lspconfig.nvim' },
   { 'neovim/nvim-lspconfig' },
@@ -71,31 +53,14 @@ require("lazy").setup({
   {
     "L3MON4D3/LuaSnip",
     version = "v2.*",
-    build = "make install_jsregexp", -- ensure `make` and Node.js are installed
+    build = "make install_jsregexp",
   },
-  {
-    'ray-x/lsp_signature.nvim',
-    event = "VeryLazy",
-    opts = {}, -- lazy.nvim will automatically call setup(opts)
-  },
+  
+  -- ===== Editing Enhancements =====
+  { 'windwp/nvim-autopairs', event = "InsertEnter", config = true },
+  { "numToStr/Comment.nvim", opts = {}, lazy = false },
 
-  -- ========================
-  -- Editing Enhancements
-  -- ========================
-  {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    config = true,
-  },
-  {
-    "numToStr/Comment.nvim",
-    opts = {},
-    lazy = false,
-  },
-
-  -- ========================
-  -- Trouble (Diagnostics)
-  -- ========================
+  -- ===== Trouble (Diagnostics) =====
   {
     "folke/trouble.nvim",
     opts = {},
@@ -103,16 +68,9 @@ require("lazy").setup({
     keys = {
       { "<leader>tt", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
       { "<leader>tT", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics" },
-      { "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "Symbols (Trouble)" },
       { "<leader>cl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", desc = "LSP Definitions/References" },
       { "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location List" },
       { "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List" },
     },
   },
-
-  -- ========================
-  -- Fun / Misc
-  -- ========================
-  { 'ThePrimeagen/vim-be-good', lazy = true },
-  { 'ThePrimeagen/vim-apm', lazy = true },
 })
--- nvim/lua/set.lua
+++ nvim/lua/set.lua
@@ -1,33 +1,32 @@
+-- Appearance
 vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20"
 vim.opt.colorcolumn = "80"
+vim.opt.termguicolors = true
 
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+vim.opt.updatetime = 50
+
+-- Line numbers
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-
+-- Tabs & indentation
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
+-- Swap/backup/undo
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
+-- Search
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
-
-vim.opt.termguicolors = true
-
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-
-vim.opt.isfname:append("@-@")
-
-vim.opt.updatetime = 50
-
